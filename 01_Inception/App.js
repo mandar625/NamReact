2943b53d3245c851c4c01082c2f566ced47cdf92
Mandar Ramkrushna Marathe <MRMarathe@bpl.netlink.com>
2023-04-09 00:59:51 +0530

    foundation

--- 01_Inception/App.js
+++ 01_Inception/App.js
@@ -15,32 +15,4 @@
  */
 import React from "react";
 import  ReactDOM  from "react-dom";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [ React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "Hellow this is done by nested  html structure React"),
-    React.createElement("h2", {}, "Hellow this is done by nested  html structure React")]
-  ), React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "Hellow this is done by nested  html structure React"),
-    React.createElement("h2", {}, "Hellow this is done by nested  html structure React")]
-  )]
 
- 
-);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", className: "headingClass" },
-//   "hiii I am h1 By React"
-// );
-
-// console.log(heading); // object
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
